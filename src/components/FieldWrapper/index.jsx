c8c1de8fa65eb0281ba085ff50a39e14c8c74215
gabruw <gabrielmarques.mg@gmail.com>
2020-11-26 20:01:57 -0300

    fix: Correção no FieldWrapper

--- src/components/FieldWrapper/index.jsx
+++ src/components/FieldWrapper/index.jsx
@@ -4,22 +4,24 @@ import React, { useMemo } from 'react';
 import { Controller } from 'react-hook-form';
 import styles from './styles.module.css';
 
+import '../../assets/css/fonts.css';
+
 //#endregion
 
-const FieldWrapper = ({ as: Comp, name, errors, label, className, ...rest }) => {
+const FieldWrapper = ({ as, name, errors, label, className, ...rest }) => {
     const error = useMemo(() => errors && errors[name], [errors, name]);
 
     return (
         <div className={styles.content}>
             <div className={styles.input}>
                 <Controller
+                    as={as}
                     name={name}
                     defaultValue=''
                     variant='outlined'
                     error={Boolean(error)}
                     style={{ display: 'flex' }}
                     className={styles.controller}
-                    as={<Comp className={styles.component} />}
                     label={<div className={styles.label}>{label}</div>}
                     {...rest}
                 />
