5c0fe5cb69449388e02fb5fc51459f278c9e716e
gabruw <gabrielmarques.mg@gmail.com>
2020-11-30 00:41:17 -0300

    fix: Correções no schema do FormRegister

--- src/views/authentication/FormRegistration/schema.js
+++ src/views/authentication/FormRegistration/schema.js
@@ -10,17 +10,17 @@ import yup, { emptyStringToUndefined } from 'utils/validations/yup';
 //#endregion
 
 const registrationSchema = yup.object().shape({
-    [USER_FIELDS.CPF]: yup.string().required().max(255).label(USER_LABELS.CPF),
     [USER_FIELDS.NAME]: yup.string().required().max(255).label(USER_LABELS.NAME),
+    [USER_FIELDS.CPF]: yup.string().required().min(14).max(14).label(USER_LABELS.CPF),
+    [AUTHENTICATION_FIELDS.PASSWORD]: yup.string().required().min(6).max(40).label(AUTHENTICATION_LABELS.PASSWORD),
+    [AUTHENTICATION_FIELDS.EMAIL]: yup.string().email().required().min(6).max(80).label(AUTHENTICATION_LABELS.EMAIL),
     [USER_FIELDS.BIRTH]: yup
         .string()
         .required()
         .transform(emptyStringToUndefined)
-        .max(255)
+        .max(10)
         .test('Idade', 'A idade deve ser superior a 18 anos', (value) => moment().diff(value, 'years') > 18)
-        .label(USER_LABELS.BIRTH),
-    [AUTHENTICATION_FIELDS.PASSWORD]: yup.string().required().max(255).label(AUTHENTICATION_LABELS.PASSWORD),
-    [AUTHENTICATION_FIELDS.EMAIL]: yup.string().email().required().max(255).label(AUTHENTICATION_LABELS.EMAIL)
+        .label(USER_LABELS.BIRTH)
 });
 
 export default registrationSchema;
