5f8f5c0b37f0f3ba9295ed972875ece5381117c1
diegompazos <dmarquespazos@gmail.com>
2020-12-04 12:43:18 -0300

    feat: Inclusão da busca de Questões para as provas

--- src/views/test/services/get-data.js
+++ src/views/test/services/get-data.js
@@ -2,9 +2,12 @@
 
 import API from 'api/api';
 import ENDPOINT from 'api/endpoint';
+import { findAllQuestions } from 'views/question/services/get-data';
 
 //#endregion
 
 export const findTest = (id) => API.get(ENDPOINT.TEST.FIND(id));
 
 export const findAllTests = (page, order, direction) => API.get(ENDPOINT.TEST.FIND_ALL(page, order, direction));
+
+export const findAllQuestions;
