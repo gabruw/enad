1cf888d69d250c4a842ffcd1c2c3c03895d4541f
diegompazos <dmarquespazos@gmail.com>
2020-12-04 13:03:39 -0300

    fix: Correção do get-data.

--- src/views/test/services/get-data.js
+++ src/views/test/services/get-data.js
@@ -2,12 +2,9 @@
 
 import API from 'api/api';
 import ENDPOINT from 'api/endpoint';
-import { findAllQuestions } from 'views/question/services/get-data';
 
 //#endregion
 
 export const findTest = (id) => API.get(ENDPOINT.TEST.FIND(id));
 
 export const findAllTests = (page, order, direction) => API.get(ENDPOINT.TEST.FIND_ALL(page, order, direction));
-
-export const findAllQuestions;
